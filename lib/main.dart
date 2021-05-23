488a8fc1fd5cc89e33dbab679dbab2791aa1a747
Duncan Moyo <duncanfmoyo@gmail.com>
2021-05-23 11:12:37 +0200

    onGenerateRoute and onUnknownRoute

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class MyApp extends StatelessWidget {
               headline6: TextStyle(
                 fontSize: 20,
                 fontFamily: 'RobotoCondensed',
-                fontWeight: FontWeight.bold,  
+                fontWeight: FontWeight.bold,
               ),
             ),
       ),
@@ -52,6 +52,22 @@ class MyApp extends StatelessWidget {
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
       },
+      // onGenerateRoute: (settings) {
+      //   print(settings.arguments);
+        // if (settings.name == '/meal-detail') {
+        //   return ...
+        // } else if (settings.name == '/something-else') {
+        //   return ...
+        // }
+        // return MaterialPageRoute(
+        //   builder: (ctx) => CategoriesScreen(),
+        // );
+      // },
+      onUnknownRoute: (settings) {
+         return MaterialPageRoute(
+          builder: (ctx) => CategoriesScreen(),
+        );
+      } ,
     );
   }
 }
